424dc1cee2041eb3c8ef5970edac2bb812caf8d8
Ivan O <ioparin@pm.me>
2023-04-15 05:41:45 +0400

    calculating wallet dashboard layout type

--- Sources/app-ios/App/Modules/Wallet/Presentation/WalletDashboard/WalletDashboardViewState.swift
+++ Sources/app-ios/App/Modules/Wallet/Presentation/WalletDashboard/WalletDashboardViewState.swift
@@ -1,18 +1,16 @@
 import Combine
-//
-//case baseToken
-//case fungibleToken
-//case nonFungibleToken
-//case nonLiquidAsset
 
 @MainActor
 final class WalletDashboardViewState: PerduxViewState {
 	private var pipelines: Set<AnyCancellable> = []
 	
-	@Published var baseTokens: [BaseToken] = []
-	@Published var fungibleTokens: [FungibleToken] = []
-	@Published var nonFungibleTokens: [NonFungibleToken] = []
-	@Published var nonLiquidAssets: [NonLiquidAsset] = []
+	@Published private(set) var baseTokens: [BaseToken] = []
+	@Published private(set) var fungibleTokens: [FungibleToken] = []
+	@Published private(set) var nonFungibleTokens: [NonFungibleToken] = []
+	@Published private(set) var nonLiquidAssets: [NonLiquidAsset] = []
+	
+	@Published private(set) var extendedLayout: TokenLayout = .aggregated
+	
 	
 	init(walletState: WalletState) {
 		initPipelines(walletState: walletState)
@@ -29,6 +27,36 @@ final class WalletDashboardViewState: PerduxViewState {
 				self?.nonLiquidAssets = assetsByType[.nonLiquidAsset] as? [NonLiquidAsset] ?? []
 			}
 			.store(in: &pipelines)
+		
+		Publishers.Zip4($baseTokens, $fungibleTokens, $nonFungibleTokens, $nonLiquidAssets)
+			.map { base, ft, nft,  nlt in
+				print("count: \(base.count), \(ft.count), \(nft.count), \(nlt.count)")
+				if (base.count + ft.count + (nft.count * 2) + nlt.count) >= 10 {
+					if base.count + ft.count + nlt.count >= 10 {
+						return .discrete
+					} else {
+						return .hybrid
+					}
+				}
+				return .aggregated
+			}
+			.receive(on: DispatchQueue.main)
+			.assign(to: &$extendedLayout)
+		
+		$extendedLayout
+			.removeDuplicates()
+			.sink { layout in
+				print("layout: \(layout)")
+			}
+			.store(in: &pipelines)
+	}
+}
+
+extension WalletDashboardViewState {
+	enum TokenLayout {
+		case aggregated
+		case hybrid
+		case discrete
 	}
 }
 
